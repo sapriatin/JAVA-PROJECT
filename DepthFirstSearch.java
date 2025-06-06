52177e3729ebeaaa994201fdf8853d2d61385614
sapriatin <sapriatin03@gmail.com>
2025-06-07 00:32:11 +0800

    Add files via upload

---
+++ DepthFirstSearch.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class Graph {
+    private final int V;
+    private final LinkedList<Integer>[] adj;
+
+    @SuppressWarnings("unchecked")
+    Graph(int v) {
+        V = v;
+        adj = (LinkedList<Integer>[]) new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList<>();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print("a" + (v + 1) + " ");
+        
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    void DFS(int v) {
+        boolean visited[] = new boolean[V];
+        System.out.println("DFS dengan vertex awal a" + (v + 1));
+        DFSUtil(v, visited);
+    }
+
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<>();
+        
+        visited[s] = true;
+        queue.add(s);
+        
+        System.out.println("BFS dengan vertex awal a" + (s + 1));
+        
+        while (!queue.isEmpty()) {
+            s = queue.poll();
+            System.out.print("a" + (s + 1) + " ");
+            
+            for (Integer n : adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Graph g = new Graph(7);
+        
+        // Membuat hubungan antar node
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+        g.addEdge(4, 6);
+        
+        System.out.println("Struktur Graf:");
+        System.out.println("a1 -> a2, a3");
+        System.out.println("a2 -> a4, a5");
+        System.out.println("a3 -> a6");
+        System.out.println("a5 -> a7");
+        System.out.println();
+        
+        System.out.println("=== Hasil DFS ===");
+        g.DFS(0);
+        
+        System.out.println("\n\n=== Hasil BFS ===");
+        g.BFS(0);
+    }
+}
